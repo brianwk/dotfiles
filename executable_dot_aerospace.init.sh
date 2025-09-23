c86131d2fb068f0dd6e12065fb822a15a2530d56
Brian Kaplan <brian@softwaredeployed.com>
2025-09-23 13:38:24 -0400

    latest changes

--- executable_dot_aerospace.init.sh
+++ executable_dot_aerospace.init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+borders active_color=0xcccc5500 inactive_color=0x66f0f0f0 width=8.0 style=square 2>&1 > /dev/null &
 killall AutoRaise
 /Applications/AutoRaise.app/Contents/MacOS/AutoRaise 2>&1 > /dev/null &
 killall AerospaceSwipe
