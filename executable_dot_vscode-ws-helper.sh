d8866ad826b52cbf299407ad3a361fe8d88023b6
Brian Kaplan <brian@softwaredeployed.com>
2025-10-08 15:55:41 -0400

    add catpuccin powerline

--- executable_dot_vscode-ws-helper.sh
+++ executable_dot_vscode-ws-helper.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 # set var workspace which is the argument sent to the script
-#local workspace=$1
-#aerospace move-node-to-workspace $workspace 2>&1 >/dev/null
+#aerospace enable off
+#aerospace enable on
+local workspace=$1
+aerospace move-node-to-workspace $workspace 2>&1 >/dev/null
 #aerospace workspace $workspace 2>&1 >/dev/null
