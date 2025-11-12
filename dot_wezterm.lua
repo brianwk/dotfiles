98fc97081946473a7cedcae02efd009f89b6248d
Brian Kaplan <brian@softwaredeployed.com>
2025-11-11 22:09:24 -0500

    latest changes

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,6 +5,6 @@ config.font_size = 15
 config.font = wezterm.font('Hack Nerd Font', { weight = 'Bold' })
 config.color_scheme = 'Catppuccin Frappe'
 config.window_decorations = "RESIZE"
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 
 return config
