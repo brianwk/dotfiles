00ddb5b7242845f495dce38d5afa2f4e2530858c
Brian Kaplan <brian@softwaredeployed.com>
2025-11-11 22:27:38 -0500

    disable hotkey for new tab so i don't accidentally open a tab

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,6 +5,14 @@ config.font_size = 15
 config.font = wezterm.font('Hack Nerd Font', { weight = 'Bold' })
 config.color_scheme = 'Catppuccin Frappe'
 config.window_decorations = "RESIZE"
-config.enable_tab_bar = true
-
+config.enable_tab_bar = false 
+config.keys = {
+  -- Disable the default "New Tab" hotkey (example for Ctrl+Shift+T)
+  {
+    key = 't',
+    mods = 'SUPER',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  -- Add other custom key assignments below this line if needed
+}
 return config
