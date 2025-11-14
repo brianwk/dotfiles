753bec352ab7051d9dcd319ef404a203ead92bc8
Brian Kaplan <brian@softwaredeployed.com>
2025-11-14 11:49:48 -0500

    update sketchybar config, seems cursed because i can't reload the config without it exploding

--- private_dot_config/sketchybar/plugins/executable_rift.sh
+++ private_dot_config/sketchybar/plugins/executable_rift.sh
@@ -3,7 +3,7 @@
 # make sure it's executable with:
 # chmod +x ~/.config/sketchybar/plugins/rift.sh
 
-RIFT_CLI="/Users/briankaplan/dev-current/rift/bin/rift-cli"
+RIFT_CLI="/Users/briankaplan/dev-current/rift/target/debug/rift-cli"
 
 # If a specific workspace was passed as argument, handle its visibility
 if [ -n "$1" ]; then
@@ -11,8 +11,11 @@ if [ -n "$1" ]; then
     # parse name space.x.y where x is workspace ID and y is monitor ID
     monitor=$(echo $NAME | cut -d'.' -f3)
     display_uuid=$(sketchybar --query displays | jq -r '.[] | select(."arrangement-id" == '$monitor') | .UUID')
+    echo "uuid:" $display_uuid;
     display_space=$($RIFT_CLI query displays | jq -r '.[] | select(.uuid == "'$display_uuid'") | .space')
+    echo "display space: " $display_space;
     visible_workspace=$($RIFT_CLI query workspaces --space-id "$display_space" | jq -r '.[] | select(.is_active) | .index')
+    echo "visible: " $visible_workspace;
     if [ "$workspace_id" = "$visible_workspace" ]; then
       sketchybar --set $NAME background.color=0x88cc5500
     else
