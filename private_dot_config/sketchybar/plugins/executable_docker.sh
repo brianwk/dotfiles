8abf1ebb4c560c7711fb4c08dfdd005d499078b4
Brian Kaplan <brian@softwaredeployed.com>
2025-11-16 21:45:27 -0500

    add system woke event to docker item

--- private_dot_config/sketchybar/plugins/executable_docker.sh
+++ private_dot_config/sketchybar/plugins/executable_docker.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+num_containers=0
+
 if colima status native > /dev/null; then 
-  sketchybar --set docker icon.drawing=on
+  num_containers=$(docker info -f json | jq '.ContainersRunning')
+  sketchybar --set docker icon.drawing=on label.drawing=on
 else
   sketchybar --set docker icon.drawing=off
 fi
+
+sketchybar --set docker label="$num_containers"
