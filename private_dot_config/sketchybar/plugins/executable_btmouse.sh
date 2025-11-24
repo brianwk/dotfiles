2818226688cd70316682f3b42dcf87e322bc3df6
Brian Kaplan <brian@softwaredeployed.com>
2025-11-24 15:46:02 -0500

    add battery percentage to sketchybar for mouse & headphones

--- private_dot_config/sketchybar/plugins/executable_btmouse.sh
+++ private_dot_config/sketchybar/plugins/executable_btmouse.sh
@@ -1,7 +1,9 @@
 MOUSE_CONNECTED=$(blueutil --info "MX Anywhere 3S" --format json | jq '.connected')
+BATTERY_PERCENT=$(pmset -g accps|grep 'MX Anywhere 3S' | cut -f '2-3'|ggrep -Po '[0-9]+')
 
 if [[ "$MOUSE_CONNECTED" == "true" ]]; then
-   sketchybar --set btmouse drawing=on label.drawing=off icon="󰦋" icon.font.size=24
+   sketchybar --set btmouse drawing=on label.drawing=off icon="󰦋" icon.font.size=24 label.padding_right=0
+   sketchybar --set btmouse_battery drawing=on label="$BATTERY_PERCENT%" label.color=0x22f0f0f0f0 background.drawing=off label.font.size=10 label.y_offset=-10 label.padding_left=0
 else
    sketchybar --set btmouse drawing=off
 fi
--- private_dot_config/sketchybar/plugins/executable_headphones.sh
+++ private_dot_config/sketchybar/plugins/executable_headphones.sh
@@ -1,9 +1,11 @@
 
 AUDIO_OUTPUT_DEVICE=$(SwitchAudioSource -c)
 BLUETOOTH_DEVICE=$(blueutil --paired --format json|jq -r '.[] | select(.name == "ATH-M50xBT2") | .name')
-
+BATTERY_PERCENT=$(pmset -g accps|grep 'ATH-M50xBT2' | cut -f '2-3'|ggrep -Po '[0-9]+')
 if [[ "$AUDIO_OUTPUT_DEVICE" == "$BLUETOOTH_DEVICE" ]]; then
-   sketchybar --set headphones drawing=on label="$AUDIO_OUTPUT_DEVICE" icon="󰥰" icon.font.size=32
+  sketchybar --set headphones drawing=on label="$AUDIO_OUTPUT_DEVICE" icon="󰥰" icon.font.size=32 background.drawing=off
+  sketchybar --set headphones_battery drawing=on label="$BATTERY_PERCENT%" label.color=0x22f0f0f0f0 background.drawing=off label.font.size=10 label.y_offset=-10 label.padding_left=0
 else
    sketchybar --set headphones drawing=off
+   sketchybar --set headphones_battery drawing=off
 fi
