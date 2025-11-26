22e6ede34badc5cbe4d1d8a89b65cb2af1492998
Brian Kaplan <brian@softwaredeployed.com>
2025-11-26 12:31:58 -0500

    latest updates

--- executable_dot_motd.sh
+++ executable_dot_motd.sh
@@ -1,5 +1,5 @@
 FIGLET_FONT="/Users/briankaplan/.config/phm-vga.flf"
-if [[ $TERM_PROGRAM == "vscode" ]]; then
+if [[ $TERM_PROGRAM == "vscode" || $TERM_PROGRAM == "zed" ]]; then
   FIGLET_FONT="banner3"
 fi
 figlet -C utf8 -f $FIGLET_FONT -k -W -w 100 -c "Software Deployed" | lolcat -t -S 19880623
