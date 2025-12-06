6903edc8ab9412a5ea50086812fbb72122d1f053
Brian Kaplan <brian@softwaredeployed.com>
2025-12-06 14:47:21 -0500

    latest updates

---
+++ dot_bin/executable_zws.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+# 
+#❯ zellij --session architary --layout ~/.config/zellij/architary.kdl
+#There is no active session!
+
+NAME=$1
+zellij --session $NAME --layout $NAME 2>1
+if [[ $? == 1 ]] then
+  zellij --session $NAME --new-session-with-layout $NAME;
+fi
