bd6d7839a96d5544618a105e89de8092321e3cc1
Brian Kaplan <brian@softwaredeployed.com>
2025-12-06 21:31:57 -0500

    latest changes

--- private_dot_config/rift/executable_rift_title_changed.sh
+++ private_dot_config/rift/executable_rift_title_changed.sh
@@ -4,7 +4,7 @@ RIFT_CLI="/Users/briankaplan/dev-current/rift/target/debug/rift-cli"
 
 #{"type":"window_title_changed","window_id":{"pid":87483,"idx":49327},"workspace_id":{"idx":10,"version":1},"workspace_index":9,"workspace_name":"9","previous_title":"Visual Studio Code","new_title":"CreatePulseButton.tsx — PulseReport — Pulse Report","space_id":4,"display_uuid":"37D8832A-2D66-02CA-B9F7-8F30A301B230"}
 
-TARGET_WINDOW_TITLES=( "Visual Studio Code" "Visual Studio Code - Insiders" )
+TARGET_WINDOW_TITLES=( "zellij" "wezterm" )
 JSON_INPUT="$@"
 
 WINDOW_ID=$(echo $RIFT_EVENT_JSON | jq -r '.window_id.idx')
@@ -22,11 +22,11 @@ fi
 
 WORK_DISPLAY_UUID="35D8FFE8-6A4B-45DF-BB20-14D3D229A5B8"
 
-typeset -A profile_map=( DEFAULT 3 PulseReport 4 Architary 5 CRC 6 BGA 7 SD 8 )
-VSCODE_PROFILE=$(echo $NEW_TITLE | ggrep -oP -- '(— |^)([\w\s]+)$'|tr -d '— ')
+typeset -A profile_map=( PulseReport 4 architary 5 )
+VSCODE_PROFILE=$(echo $NEW_TITLE | ggrep -oP -- '^(\w+)')
 NEW_WORKSPACE=$profile_map[$VSCODE_PROFILE]
 if [[ -z "$NEW_WORKSPACE" ]]; then
-  NEW_WORKSPACE=8
+  exit 0
 fi
 echo "Moving $WINDOW_ID to $NEW_WORKSPACE" >> /Users/briankaplan/rift_title_changed.log
 
