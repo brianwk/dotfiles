259451692614526bf1971cfeccf4aa78fdab737f
Brian Kaplan <brian@softwaredeployed.com>
2025-12-09 15:46:38 -0500

    don't use a for loop in wezterm config, use env vars, thankfully skhd supports this

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -45,12 +45,12 @@ config.keys = {
 local mux = wezterm.mux
 
 function make_shell_pane(parent_pane, cmd, direction, size)
-  local size = size or 0.10
+  local size = size or 0.05
   local shell_pane = parent_pane:split { size = size, direction = direction, args = cmd }
   return shell_pane
 end
 
-function setup (editor_pane, project)
+function setup (editor_pane, project, cwd)
    if project == "architary" then
      local shell_pane = make_shell_pane(editor_pane, { "yarn", "dev" }, "Bottom")
      local convex_pane = make_shell_pane(shell_pane, { "yarn", "dlx",  "convex", "dev" }, "Right", 0.05)
@@ -60,9 +60,16 @@ function setup (editor_pane, project)
      local shell_pane = make_shell_pane(editor_pane, { "yarn", "dlx", "expo", "run:ios", "-d", }, "Bottom")
      local gitui_pane = make_shell_pane(editor_pane, { "gitui" }, "Right", 0.05)
    end
+  if project == "PulseReportFastApi" then
+     local shell_pane = make_shell_pane(editor_pane, { cwd .. "/.venv/bin/uvicorn", "main:app", "--port", "8888", "--reload", "--log-level", "debug", "--host", "0.0.0.0" }, "Bottom")
+     local gitui_pane = make_shell_pane(editor_pane, { "gitui" }, "Right", 0.05)
+  end
 end
 
 local projects = {
+ PulseReportFastApi = {
+   cwd = wezterm.home_dir .. "/dev-current/treyapp/PulseReportFastApi"
+ },
  PulseReport = {
    cwd = wezterm.home_dir .. "/dev-current/treyapp/PulseReport"
  },
@@ -71,16 +78,28 @@ local projects = {
  }
 }
 
+function get_config()
+   local workspace = os.getenv("WORKSPACE")
+   for project, config in pairs(projects) do
+     if project == workspace then
+       return config
+      end
+    end
+end
+
 wezterm.on("gui-startup", function(cmd)
-    for project, config in pairs(projects) do
+    local project = os.getenv("WORKSPACE")
+    local config = get_config()
+    if config then
+      -- for project, config in pairs(projects) do
       local tab, editor_pane, window = mux.spawn_window({
           workspace = project,
           cwd = config.cwd,
-          args = { "hx", config.cwd }, --cmd and cmd.args,
+          args = { "hx", config.cwd },
           size = 0.75
       })
       window:set_title(project)
-      setup(editor_pane, project)
+      setup(editor_pane, project, config.cwd)
     end
 end)
 
