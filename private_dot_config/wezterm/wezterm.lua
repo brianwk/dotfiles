f42ffa75ae49a4b0511d5dccaca8bf130390a4b6
Brian Kaplan <brian@softwaredeployed.com>
2025-12-10 00:17:47 -0500

    latest updates

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ config.front_end = "WebGpu"
 config.max_fps = 144
 config.font_size = 12 
 config.font = wezterm.font('Hack Nerd Font', { weight = 'Bold' })
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = 'Catppuccin Frappe'
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false 
 config.keys = {
@@ -45,7 +45,7 @@ config.keys = {
 local mux = wezterm.mux
 
 function make_shell_pane(parent_pane, cmd, direction, size)
-  local size = size or 0.05
+  local size = size or 0.01
   local shell_pane = parent_pane:split { size = size, direction = direction, args = cmd }
   return shell_pane
 end
@@ -57,24 +57,56 @@ function setup (editor_pane, project, cwd)
      local gitui_pane = make_shell_pane(editor_pane, { "gitui" }, "Right", 0.05)
    end
    if project == "PulseReport" then
-     local shell_pane = make_shell_pane(editor_pane, { "yarn", "dlx", "expo", "run:ios", "-d", }, "Bottom")
+     local shell_pane = make_shell_pane(editor_pane, { "./start-expo.sh", }, "Bottom")
      local gitui_pane = make_shell_pane(editor_pane, { "gitui" }, "Right", 0.05)
    end
   if project == "PulseReportFastApi" then
      local shell_pane = make_shell_pane(editor_pane, { cwd .. "/.venv/bin/uvicorn", "main:app", "--port", "8888", "--reload", "--log-level", "debug", "--host", "0.0.0.0" }, "Bottom")
      local gitui_pane = make_shell_pane(editor_pane, { "gitui" }, "Right", 0.05)
   end
+  if project == "PulseReportWebsite" then
+    local shell_pane = make_shell_pane(editor_pane, { "pnpm", "dev" }, "Bottom")
+    local gitui_pane = make_shell_pane(editor_pane, { "gitui" }, "Right", 0.25)
+  end
+  if project == "CRC" or project == "BGA" then
+    -- PHP projects
+    local shell_pane = make_shell_pane(editor_pane, { "zsh" }, "Bottom")
+    local gitui_pane = make_shell_pane(editor_pane, { "gitui" }, "Right", 0.05)
+  end
+  if project == "executor" then
+    local shell_pane = make_shell_pane(editor_pane, { "pnpx", "rescript", "watch"}, "Bottom")
+    local gitui_pane = make_shell_pane(editor_pane, { "gitui" }, "Right", 0.05)
+  end
 end
 
 local projects = {
+ PulseReportWebsite = {
+   title = "PulseReport",
+   cwd = wezterm.home_dir .. "/dev-current/treyapp/pulse-report-invite-site" 
+ },
  PulseReportFastApi = {
+   title = "PulseReport",
    cwd = wezterm.home_dir .. "/dev-current/treyapp/PulseReportFastApi"
  },
  PulseReport = {
+   title = "PulseReport",
    cwd = wezterm.home_dir .. "/dev-current/treyapp/PulseReport"
  },
  architary = {
+   title = "architary",
    cwd = wezterm.home_dir .. "/dev-current/architary/architary-nextjs"
+ },
+ CRC = {
+   title = "CRC",
+   cwd = wezterm.home_dir .. "/dev-current/crc"
+ },
+ BGA = {
+   title = "BGA",
+   cwd = wezterm.home_dir .. "/dev-current/bga"
+ },
+ executor = {
+   title = "executor",
+   cwd = wezterm.home_dir .. "/dev-current/executor-full-stack"
  }
 }
 
@@ -98,9 +130,18 @@ wezterm.on("gui-startup", function(cmd)
           args = { "hx", config.cwd },
           size = 0.75
       })
-      window:set_title(project)
       setup(editor_pane, project, config.cwd)
     end
+end)
+
+
+wezterm.on('format-window-title', function ()
+	local config = get_config()
+	if config then
+    local title = config.title or 'Wezterm'
+    return title
+  end
+  return 'Wezterm'
 end)
 
 return config
